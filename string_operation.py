711c6b2230f576f95cbda9f88469bde5f832b22b
hiral2011 <110373383+hiral2011@users.noreply.github.com>
2022-08-27 18:00:31 +0530

    Add files via upload

---
+++ string_operation.py
@@ -0,0 +1,59 @@
+# var1 = 'Hello World!'
+# var2 = "Python Programming"
+
+# print ("var1[0]: ", var1[0])
+# print ("var2[1:5] :", var2[1:5])
+
+# list = ['physics', 'chemistry', 1997, 2000]
+# print ("Value available at index 2 : ", list[2])
+
+# list[2] = 2001
+# print ("New value available at index 2 : ", list
+
+
+# list = ['physics', 'chemistry', 1997, 2000]
+# print (list)
+
+# del list[2]
+# print (list)
+
+
+# L = ['C++', 'Java', 'Python','physics', 'chemistry']
+# L2= [1997, 2000]
+# print(max(L))
+# print(max(L2))
+
+# L = ['C++', 'Java', 'Python','physics', 'chemistry']
+# L2= [1997, 2000]
+# print(max(L))
+# print(max(L2))
+
+# L = ['C++', 'Java', 'Python','physics', 'chemistry']
+# L2= [1997, 2000]
+# L.extend(L2)
+# print(L)
+# # print(max(L2))
+
+
+# list1 = ['physics', 'Biology', 'chemistry', 'maths']
+# list1.remove('Biology')
+# print ("list now : ", list1)
+# list1.remove('maths')
+# print ("list now : ", list1)
+
+
+# letters = ["a","b","c","d","e","f"]
+# matrix =[[0,1],[2,3]]
+# zeros = [1]*5
+# combined = zeros + letters
+# numbers =list(range(20+1))
+# chars = list("hello world")
+# # print(combined)
+# print(numbers[::-1])
+
+
+
+l1 = ["p","y","t","h","o","n"]
+length = len(l1)
+for i in range(length):
+    print(i,l1[i])
